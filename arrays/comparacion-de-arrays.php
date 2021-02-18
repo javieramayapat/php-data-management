d94d172a79807b1c7fd5b6d67dc92b0d88988b1e
Javier Amaya Patricio <javieramayapat@gmail.com>
2021-02-17 23:14:12 -0600

    Comparación de arrays php

---
+++ arrays/comparacion-de-arrays.php
@@ -0,0 +1,14 @@
+<?php
+
+$cursos = ['php','html','css','javascript','python','django'];
+$listaDeCursosParaEsteAño = ['php','javascript','html','css'];
+
+echo "<pre>";
+// var_dump(array_diff($cursos,$listaDeCursosParaEsteAño));
+
+$arrayA = [1,2,3,4,5,6];
+$arrayB = [4,5,6,7,8,9];
+
+// var_dump(array_diff($arrayA,$arrayB)); //Salida 1,2,3
+// var_dump(array_diff($arrayB,$arrayA)); //Salida 7,8,9
+
